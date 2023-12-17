42c5302c68fa9ef2b8012abe623b85f87fcad7e3
david-lafontant <lafontant.david@gmail.com>
2023-12-17 17:05:40 -0500

    Get rockets data with axios

--- src/app/rockets/rocketsSlice.ts
+++ src/app/rockets/rocketsSlice.ts
@@ -1,6 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from 'axios';
 
  const initialState = {};
+ const ROCKET_URL = 'https://api.spacexdata.com/v3/rockets';
+
+ CONST fetchRockets = createAsyncThunk(
+  'rockets/fetchRockets',
+  async () => {
+    const response = await axios.get(ROCKET_URL);
+    return response.data
+  }
+ )
 
  const rocketSlice = createSlice({
   name: 'rockets',
