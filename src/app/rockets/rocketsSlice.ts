bfc854c8301d590e208f288ebf557d10eb999c7b
david-lafontant <lafontant.david@gmail.com>
2023-12-18 22:01:24 -0500

    Setup rocketSlice extraReducers

--- src/app/rockets/rocketsSlice.ts
+++ src/app/rockets/rocketsSlice.ts
@@ -1,10 +1,22 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from 'axios';
 
- const initialState = {};
+interface rocketState {
+  rockets: []
+  status: 'idle' | 'pending' | 'succeeded' | 'failed',
+  error: string | null | undefined
+}
+
+
+ const initialState = {
+  rockets: [],
+  status: 'idle',
+  error: null,
+ } as rocketState;
+
  const ROCKET_URL = 'https://api.spacexdata.com/v3/rockets';
 
- CONST fetchRockets = createAsyncThunk(
+ export const fetchRockets = createAsyncThunk(
   'rockets/fetchRockets',
   async () => {
     const response = await axios.get(ROCKET_URL);
@@ -16,7 +28,18 @@ import axios from 'axios';
   name: 'rockets',
   initialState,
   reducers: {},
-  extraReducers: () => {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchRockets.pending, (state) => {
+      state.status = 'pending';
+    }).addCase(fetchRockets.fulfilled, (state, action) => {
+           state.status = 'succeeded';
+    state.rockets = action.payload;
+    
+    }).addCase(fetchRockets.rejected, (state, action)=>{
+      state.status = 'failed';
+      state.error = action.error.message;
+    });
+  },
  });
 
  export default rocketSlice.reducer;
