763eb5c4a869bc6d7105ec79d4322fec95d575a0
david-lafontant <lafontant.david@gmail.com>
2023-12-18 22:03:35 -0500

    Import Rockets component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import './App.css'
+import Rockets from "./pages/Rockets"
 
 function App() {
+
   return (
     <>
-      <p className="font-bold text-center">Hello world!</p>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1> 
+      <h1>main page</h1>
+      <Rockets />
+
     </>
   )
 }
 
 export default App
+
+
