6de070b10518a37e8e879b0a3c77747f2bc5729b
david-lafontant <lafontant.david@gmail.com>
2023-12-22 07:33:09 -0500

    Import mission reducer in store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rocketsSlice from "./rockets/rocketsSlice";
 import dragonsSlice from "./dragons/dragonsSlice";
+import missionsSlice from "./missions/missionsSlice";
 
 export const store = configureStore({
   reducer:{
     rockets: rocketsSlice,
     dragons: dragonsSlice,
+    missions: missionsSlice
 
   },
 });
