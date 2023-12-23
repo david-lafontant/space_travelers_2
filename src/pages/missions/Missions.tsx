9b15b86589a9721bce94c22d87d3b19179673101
david-lafontant <lafontant.david@gmail.com>
2023-12-23 09:58:26 -0500

    Import and dsiplay missions

--- src/pages/missions/Missions.tsx
+++ src/pages/missions/Missions.tsx
@@ -4,47 +4,60 @@ import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from "../../services/hooks";
 import type { RootState } from '../../services/store';
 import { fetchMissions } from '../../services/missions/missionsSlice';
-import Mission from '../../types/mission';
+import MissionType from '../../types/mission';
+import Mission from '../../components/mission/Mission';
 
 function Missions() {
   const dispatch = useAppDispatch();
-  const dragonsList = useAppSelector((state: RootState) => state.missions.missions)
+  const missionsList = useAppSelector((state: RootState) => state.missions.missions)
   const loading = useAppSelector((state: RootState) => state.missions.status);
   const error = useAppSelector((state: RootState) => state.missions.error);
 
-useEffect(()=>{
-  dispatch(fetchMissions());
-},[dispatch])
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch])
 
 
-if (loading === 'pending') {
-  return <div>Loading...</div>;
-}
+  if (loading === 'pending') {
+    return <div>Loading...</div>;
+  }
 
-if (loading === 'failed') {
-  return <div>Error: {error}</div>;
-}
-
-
-if (loading === 'succeeded') {
+  if (loading === 'failed') {
+    return <div>Error: {error}</div>;
+  }
 
 
-  const missionItems = dragonsList.map((item: Mission) =>
-    <li key={item.mission_id}>
-      {item.mission_name}
-    </li>
-  );
+  if (loading === 'succeeded') {
+    return (
+  
+<div className="relative overflow-x-auto shadow-md sm:rounded-lg px-8 py-11">
+    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" className="px-6 py-3">
+                    Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Description
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Detail
+                </th>
+            </tr>
+        </thead>
+        <tbody>      
+  
+        {missionsList && missionsList.map((mission: MissionType) => {
+          return <Mission key={mission.mission_name} mission={mission} />
+        })}
+      
+        </tbody>
+    </table>
+</div>
+    
+    )
 
-  return (
-    <>
-<p>Mission page</p>
-      <ul>
-        {missionItems}
-      </ul>
-    </>
-  )
-
-}
+  }
 }
 
 export default Missions;
