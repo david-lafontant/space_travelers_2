0954f456f7758f5e295a42432bd25a36d3eaa455
david-lafontant <lafontant.david@gmail.com>
2023-12-22 22:25:31 -0500

    Implement lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import Dragons from "./pages/dragons/Dragons";
-import Missions from "./pages/missions/Missions";
-import Rockets from "./pages/rockets/Rockets";
 import Header from "./layout/Header";
 import ErrorBoundary from "./utils/errors/ErrorBoundary";
+import Rockets from "./pages/rockets/Rockets";
+const Dragons = lazy(()=>import("./pages/dragons/Dragons"));
+const Missions = lazy(() => import("./pages/missions/Missions"));
 
 function App() {
 
   return (
     <>
      <Header />
-
+     <Suspense fallback={<div>Loading..</div>}>
       <Routes>
+
        <Route path="/" element={<Rockets />} errorElement={<ErrorBoundary />} />
        <Route path="/dragons" element={<Dragons />} errorElement={<ErrorBoundary />} />
        <Route path="/missions" element={<Missions />} errorElement={<ErrorBoundary />} />
        <Route path="*" element={<Rockets />} errorElement={<ErrorBoundary />} />
 
       </Routes>
+      </Suspense>
      </>
   );
 }
