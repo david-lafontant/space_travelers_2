c4fea8a1594ed6cd3a0ecceaf4142f9eb63fdada
david-lafontant <lafontant.david@gmail.com>
2023-12-22 21:05:33 -0500

    Add lintks that point to the pages in header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='flex flex-row justify-center gap-8'>
+      <Link to="/">Rockets</Link>
+      <Link to="/dragons">Dragons</Link>
+      <Link to="/missions">Missions</Link>
+    </div>
   )
 }
 
